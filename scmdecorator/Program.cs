13d53049c69e1f702d268d4b150dc4daaa861bb9
cureos <anders@cureos.com>
2011-10-05 21:51:52 +0200

    Initial commit

---
+++ scmdecorator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace scmdecorator
+{
+    class Program
+    {
+        private static readonly string ApplicationName = "scmdecorator";
+
+        private class ScmInfo
+        {
+            internal string Scm;
+            internal string Folder;
+            internal string Icon;
+        }
+
+        private static readonly List<ScmInfo> ScmInfos =
+            new List<ScmInfo>
+                {
+                    new ScmInfo { Scm = "Subversion", Folder = ".svn", Icon = "svn.ico" },
+                    new ScmInfo { Scm = "Git", Folder = ".git", Icon = "git.ico" },
+                    new ScmInfo { Scm = "Mercurial", Folder = ".hg", Icon = "hg.ico" }
+                };
+
+        static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("usage: {0} [scm-folder]", ApplicationName);
+                return;
+            }
+
+            string scmFolder;
+            if (args.Length == 1)
+            {
+                scmFolder = args[0];
+            }
+            else
+            {
+                Console.Write("Enter name of SCM folder: ");
+                scmFolder = Console.ReadLine() ?? String.Empty;
+            }
+
+            ScmInfo currentScmInfo;
+            try
+            {
+                if (!Directory.Exists(scmFolder))
+                {
+                    Console.WriteLine("{0}: Folder '{1}' does not exist.", ApplicationName, scmFolder);
+                    return;
+                }
+
+                var dirInfo = new DirectoryInfo(scmFolder);
+                currentScmInfo = ScmInfos.SingleOrDefault(info => dirInfo.GetDirectories(info.Folder).Length > 0);
+                if (currentScmInfo == null)
+                {
+                    Console.WriteLine("{0}: Folder '{1}' does not appear to be an SCM folder; support directory is missing", ApplicationName, scmFolder);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}: Error when accessing folder, message: {1}", ApplicationName, e.Message);
+                return;
+            }
+
+
+        }
+    }
+}
---
+++ scmdecorator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("scmdecorator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cureos AB")]
+[assembly: AssemblyProduct("scmdecorator")]
+[assembly: AssemblyCopyright("Copyright © Cureos AB 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ccc9b84-b562-4ffd-958c-77c6658774d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
